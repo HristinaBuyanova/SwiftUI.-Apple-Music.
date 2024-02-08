60ff89c0b27a434c2ed0ea4777a9087f64ed8c0e
HristinaBuyanova <hristina-a@mail.ru>
2024-02-08 15:04:26 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Христина Буянова on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
