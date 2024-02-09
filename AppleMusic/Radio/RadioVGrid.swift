9883780f06879a7de342c3670bcfbbd91c6dad16
HristinaBuyanova <hristina-a@mail.ru>
2024-02-09 22:34:44 +0300

    Create vGrid

--- AppleMusic/Radio/RadioVGrid.swift
+++ AppleMusic/Radio/RadioVGrid.swift
@@ -2,8 +2,48 @@
 import SwiftUI
 
 struct RadioVGrid: View {
+
+    @State private var dataStation = StationModel.data
+    let colums = [
+        GridItem(.flexible())
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            Text("Cтанции")
+                .font(.title)
+                .bold()
+                .padding(.trailing, 250)
+
+            LazyVGrid(columns: colums) {
+
+                ForEach(dataStation) { station in
+
+                    HStack {
+                        Image(station.imageName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 100)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        VStack (alignment: .leading) {
+                            Text(station.title)
+                                .bold()
+
+                            Text(station.description)
+                                .foregroundStyle(.gray)
+
+                        }
+                        .padding()
+
+                        Spacer()
+
+                    }
+                    .padding(.leading, 10)
+
+                    Divider()
+                }
+            }
+        }
     }
 }
 
