ecaa83dae1bd037b66f72a2a441bd51f44e26f2a
HristinaBuyanova <hristina-a@mail.ru>
2024-02-17 13:55:51 +0300

    Search main view

--- AppleMusic/Search/SeachView.swift
+++ AppleMusic/Search/SeachView.swift
@@ -5,9 +5,15 @@ import SwiftUI
 struct SeachView: View {
 
     @State var tf = ""
+    @State var category = StationModel.data
+
+    var columns = [GridItem(.adaptive(minimum: 100)),
+                   GridItem(.adaptive(minimum: 100)),]
+
     var body: some View {
         NavigationStack {
             ScrollView {
+
                 TextField(
                     "Ваша медиатека"
                     , text: $tf)
@@ -17,11 +23,18 @@ struct SeachView: View {
                 .background(.silver)
                 .cornerRadius(10)
                 .padding(.horizontal, 10)
+
                 Text("Поиск по категориям")
                     .font(.title2)
                     .bold()
                     .padding(.trailing, 120)
                     .padding(.top, 10)
+
+                LazyVGrid(columns: columns, content: {
+                    ForEach(category, id: \.self) {
+                        item in CategoryCell(category: item)
+                    }
+                })
             }
             .navigationTitle("Поиск")
         }
