9b834a4f03b7f75d018d6b948dd01b590b1f5729
HristinaBuyanova <hristina-a@mail.ru>
2024-02-21 22:47:12 +0300

    Add cell for collection

---
+++ AppleMusic/UIKit/Search/DetailViewControllerHosting.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewControllerHosting.swift
+//  AppleMusic
+//
+//  Created by Христина Буянова on 21.02.2024.
+//
+
+import Foundation
---
+++ AppleMusic/UIKit/Search/SeachViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SeachViewController.swift
+//  AppleMusic
+//
+//  Created by Христина Буянова on 21.02.2024.
+//
+
+import UIKit
+
+class SeachViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
