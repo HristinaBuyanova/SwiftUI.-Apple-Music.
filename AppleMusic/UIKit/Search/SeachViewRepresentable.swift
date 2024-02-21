cfe3b5e505f153a273cc51037e4f8b2ec9680763
HristinaBuyanova <hristina-a@mail.ru>
2024-02-21 20:21:10 +0300

    Add representable

---
+++ AppleMusic/UIKit/Search/SeachViewRepresentable.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+import UIKit
+
+struct SeachViewRepresentable: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        <#code#>
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+
+}
