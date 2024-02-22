7588f2e6a6810626dca180c12b2df69a94f00dfc
HristinaBuyanova <hristina-a@mail.ru>
2024-02-22 13:51:21 +0300

    Add hosting and representable

--- AppleMusic/UIKit/Search/DetailViewControllerHosting.swift
+++ AppleMusic/UIKit/Search/DetailViewControllerHosting.swift
@@ -1,8 +1,20 @@
-//
-//  DetailViewControllerHosting.swift
-//  AppleMusic
-//
-//  Created by Христина Буянова on 21.02.2024.
-//
 
-import Foundation
+
+import SwiftUI
+
+final class DetailViewControllerHosting: UIHostingController<DetailSearchView> {
+
+    var category: SeachModel
+
+
+    init(category: SeachModel) {
+        self.category = category
+        super.init(rootView: DetailSearchView(selectModel: category))
+    }
+    
+    @objc
+    required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
