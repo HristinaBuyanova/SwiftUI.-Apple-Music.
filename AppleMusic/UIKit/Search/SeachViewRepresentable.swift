36203c066f3debe8b95fd2f28834fcd337921f77
HristinaBuyanova <hristina-a@mail.ru>
2024-02-22 13:51:39 +0300

    Representable

--- AppleMusic/UIKit/Search/SeachViewRepresentable.swift
+++ AppleMusic/UIKit/Search/SeachViewRepresentable.swift
@@ -6,7 +6,8 @@ import UIKit
 struct SeachViewRepresentable: UIViewControllerRepresentable {
 
     func makeUIViewController(context: Context) -> some UIViewController {
-        <#code#>
+        let navigationController = UINavigationController(rootViewController: SeachViewController())
+        return navigationController
     }
 
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
