24d0c3546f7cc58bcbb11903363866c87441dfe5
HristinaBuyanova <hristina-a@mail.ru>
2024-02-22 13:51:53 +0300

    Header cell

--- AppleMusic/UIKit/Search/HeaderCell.swift
+++ AppleMusic/UIKit/Search/HeaderCell.swift
@@ -1,12 +1,48 @@
-//
-//  HeaderCell.swift
-//  AppleMusic
-//
-//  Created by Христина Буянова on 22.02.2024.
-//
+
 
 import UIKit
 
 class HeaderCell: UICollectionReusableView {
-        
+    
+    static let identifier = "header"
+
+    //    MARK: - Outlets
+
+        lazy var label: UILabel = {
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+
+    //    MARK: - Init
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            clipsToBounds = true
+            setupHierarchy()
+            setupLayout()
+        }
+
+    required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+    //    MARK: - Setup
+
+        private func setupHierarchy() {
+            addSubview(label)
+        }
+
+        private func setupLayout() {
+            NSLayoutConstraint.activate([
+                label.bottomAnchor.constraint(equalTo: bottomAnchor),
+                label.leadingAnchor.constraint(equalTo: leadingAnchor)
+            ])
+        }
+
+        override func prepareForReuse() {
+            super.prepareForReuse()
+            label.text = nil
+        }
 }
